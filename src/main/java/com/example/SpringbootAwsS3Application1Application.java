a08b51792e5f48c5e1282e0205d3337109a8f92c
anupam <anupam.nitdgp14@gmail.com>
2026-02-07 21:52:26 +0530

    Initiate the project.

---
+++ src/main/java/com/example/SpringbootAwsS3Application1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsS3Application1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsS3Application1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringbootAwsS3Application1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsS3Application1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
