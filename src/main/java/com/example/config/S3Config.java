b661d97c427128dfd0715ed140092a9394f67d89
anupam <anupam.nitdgp14@gmail.com>
2026-02-08 19:02:24 +0530

    Added dev profile to access S3 from app running on EC2 instance.

--- src/main/java/com/example/config/S3Config.java
+++ src/main/java/com/example/config/S3Config.java
@@ -3,7 +3,9 @@ package com.example.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -14,20 +16,32 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Value("${cloud.aws.credentials.access-key}")
+   /* @Value("${cloud.aws.credentials.access-key}")
     private String accessKeyId;
 
     @Value("${cloud.aws.credentials.secret-access-key}")
-    private String secretAccessKey;
+    private String secretAccessKey;*/
 
-    @Bean
-    public S3Client  s3Client() {
+    @Profile("local")
+    @Bean("s3Client")
+    public S3Client s3ClientLocal(
+            @Value("${cloud.aws.credentials.access-key}") String accessKeyId,
+            @Value("${cloud.aws.credentials.secret-access-key}") String secretAccessKey) {
 
         AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
 
         return S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
+                .build();
+    }
+
+    @Profile("dev")
+    @Bean("s3Client")
+    public S3Client s3ClientDev() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.builder().build())
                 .build();
     }
 
